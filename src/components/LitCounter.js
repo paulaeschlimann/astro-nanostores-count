b91df7f800bc0383c16ce8f298fccea07f829e83
Paul Aeschlimann <paul.aeschlimann@gmail.com>
2024-07-04 22:34:49 +0200

    Modify LitCounter to use new counter store

--- src/components/LitCounter.js
+++ src/components/LitCounter.js
@@ -1,33 +1,99 @@
 import { LitElement, html } from 'lit';
-import { withStores } from '@nanostores/lit'
-import { counterStore } from '../stores/count.js'
+import { setCount, getCount } from '../stores/counter.js'
 
-export class LitCounter extends withStores(LitElement, [counterStore]) {
+export class LitCounter extends LitElement {
+  static get properties() { return {
+    count: { type: Number },
+  };}
+
+  /*static get observedAttributes() {
+    return ['count']
+  }*/
 
   constructor() {
     super()
+
+    this.count = 0
+  }
+
+  connectedCallback() {
+    super.connectedCallback()
+
+    console.log('connectedCallback')
+
+    console.log(window.localStorage.getItem('count'))
+    //this.count = getCount()
+    console.log(getCount())
+
+    //this.count = window.localStorage.getItem('count')
+    //this.count = Math.floor(Math.random() * 10)
+
+    this.count = 4
+  }
+
+  attributeChangedCallback(name, oldval, newval) {
+    console.log('attribute change: ', name, newval);
+    super.attributeChangedCallback(name, oldval, newval);
+  }
+
+  updated(changedProperties) {
+    changedProperties.forEach((oldValue, propName) => {
+      console.log(`${propName} changed. oldValue: ${oldValue}`);
+
+      if (propName === 'count') {
+        console.log('updated this.count')
+        this.count = this[propName]
+      }
+    });
+  }
+
+  update(change, second, third) {
+    super.update(change);
+    console.log(change)
+    //console.log(this[propName])
+    change.forEach((oldValue, propName) => {
+      console.log(`${propName} changed. oldValue: ${oldValue}`);
+      console.log(`new value: ${this[propName]}`)
+
+      if (propName === 'count') {
+        console.log('set this.count')
+        this.count = this[propName]
+      }
+    });
   }
 
   _increment(e) {
-    counterStore.set(counterStore.get() + 1)
+    console.log('increment')
+    //counterStore.set(counterStore.get() + 1)
   }
 
   _current(e) {
-    const current = this.count.value
+    //console.log('current')
+    const current = this.count
     console.log(`current: ${current}`)
+  }
+
+  _random(e) {
+    const random = Math.floor(Math.random() * 10)
+    console.log(`set random ${random}`)
+    this.count = random
+    //setCount(random)
   }
 
   render() {
     return html`
       <div>
         <div>
-          Count  ${counterStore.get()} (Lit)
+          Count ${this.count} (Lit)
         </div>
         <button @click="${this._increment}">
           + 1
         </button>
         <button @click="${this._current}">
           Current
+        </button>
+        <button @click="${this._random}">
+          Random
         </button>
       </div>
     `;
