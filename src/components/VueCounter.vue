04f013b6737afe9a118554caac51f2029089051c
Paul Aeschlimann <paul.aeschlimann@gmail.com>
2024-07-16 16:28:01 +0200

    Subscribe to store changes and update reactive variable

--- src/components/VueCounter.vue
+++ src/components/VueCounter.vue
@@ -1,25 +1,25 @@
 <template>
     <div className="vue-component">
       <div>
-        Count: {{$count}} (Vue)
+        Count: {{count}} (Vue)
       </div>
-      <button @click="() => counterStore.set($count + 1)">
+      <button @click="() => counterStore.set(count + 1)">
         + 1
       </button>
     </div>
   </template>
   
   <script setup lang="ts">
-  import { onMounted } from 'vue';
+  import { onMounted, ref } from 'vue';
   
   import { counterStore } from '../stores/count'
-  import { useStore } from '@nanostores/vue'
   
-  let $count = useStore(counterStore)
-  
+  const count = ref(0)
+
   onMounted(() => {
-    console.log('mount')
-    $count = useStore(counterStore)
+    counterStore.subscribe((value) => {
+      count.value = value
+    })
   })
 
   </script>
